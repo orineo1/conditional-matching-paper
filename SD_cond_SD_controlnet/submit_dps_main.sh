786dd1931fcc18ebcd4ecf950673739afda87741
Ori Meidler <orineo1@gmail.com>
2026-03-16 14:50:42 +0200

    add loss input

--- SD_cond_SD_controlnet/submit_dps_main.sh
+++ SD_cond_SD_controlnet/submit_dps_main.sh
@@ -45,6 +45,7 @@ python SD_cond_SD_controlnet/run_dps.py \
     --architect_model_id "stabilityai/sdxl-turbo" \
     --sprinter_model_id "stabilityai/sdxl-turbo" \
     --controlnet_model_id "xinsir/controlnet-scribble-sdxl-1.0" \
+    --loss_fn swd \
     --seed 1
 
 echo "DPS main pipeline complete."

a09b9a37c15c8a0a4b57ef570dbd616a6121865e
Ori Meidler <orineo1@gmail.com>
2026-04-09 19:39:33 +0300

    torch.use_deterministic_algorithms(True, warn_only=False)

--- SD_cond_SD_controlnet/run_dps.py
+++ SD_cond_SD_controlnet/run_dps.py
@@ -187,7 +187,7 @@ def set_seed(seed):
 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    torch.use_deterministic_algorithms(True, warn_only=True)
+    torch.use_deterministic_algorithms(True, warn_only=False)
     os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
 
     torch.backends.cuda.enable_flash_sdp(False)

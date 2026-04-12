b4855dd2c2ad40d58bce81631d82c029e083f238
Ori Meidler <orineo1@gmail.com>
2026-04-12 20:36:39 +0300

    fix the directory for logs

--- MNIST/train_cond.sh
+++ MNIST/train_cond.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --output=/sci/labs/orzuk/ori_m/conditional-matching-paper/logs/cond_%j.log
-#SBATCH --error=/sci/labs/orzuk/ori_m/conditional-matching-paper/logs/cond_%j.err
-#SBATCH --time=24:00:00
+#SBATCH --output=/sci/labs/orzuk/ori_m/conditional-matching-paper/MNIST/logs/uncond_%j.log
+#SBATCH --error=/sci/labs/orzuk/ori_m/conditional-matching-paper/MNIST/logs/uncond_%j.err
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=48G
@@ -13,13 +13,13 @@ source /usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/miniconda3-24
 conda activate /sci/labs/orzuk/shaulytolk/conditional-matching-paper/scribble_env
 
 export WANDB_API_KEY=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
-export REPO_PATH="${REPO_PATH:-/sci/labs/orzuk/ori_m/conditional-matching-paper}"
+export REPO_PATH="${REPO_PATH:-/sci/labs/orzuk/ori_m/conditional-matching-paper/MNIST}"
 
 echo "Job:  $SLURM_JOB_NAME  (ID: $SLURM_JOB_ID)"
 echo "Node: $(hostname)  GPU: $CUDA_VISIBLE_DEVICES"
 echo "Args: $@"
 
-mkdir -p /sci/labs/orzuk/ori_m/conditional-matching-paper/logs
+mkdir -p "$REPO_PATH/logs"
 
 cd "$REPO_PATH"
-python train_mnist_conditional.py "$@"
+python train_mnist_unconditional.py "$@"

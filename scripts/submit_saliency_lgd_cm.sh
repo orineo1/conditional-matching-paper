5bd25e2706b8242c58ccfbf580ae53f9b22306cc
Shaul Tolkowsky <stolkowsky@gmail.com>
2026-04-20 10:23:22 +0300

    Add lgd_cm saliency submit script and scribble assets

--- scripts/submit_saliency_lgd_cm.sh
+++ scripts/submit_saliency_lgd_cm.sh
@@ -12,13 +12,12 @@ export PATH="/usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/minicon
 source /usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/miniconda3-24.3.0-iqeknetqo7ngpr57d6gmu3dg4rzlcgk6/etc/profile.d/conda.sh
 conda activate /sci/labs/orzuk/shaulytolk/conditional-matching-paper/scribble_env
 
-export HF_HOME=/sci/labs/orzuk/shaulytolk/hf_cache
+export HF_HOME=/sci/labs/orzuk/shaulytolk/.cache/huggingface
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-# Clean broken numpy stub and reinstall
-SITE_PKG=/sci/labs/orzuk/shaulytolk/conditional-matching-paper/scribble_env/lib/python3.12/site-packages
-rm -rf "${SITE_PKG}/~umpy"* "${SITE_PKG}/~pmath"*
-pip install -q numpy --force-reinstall
+SITE=/sci/labs/orzuk/shaulytolk/conditional-matching-paper/scribble_env/lib/python3.12/site-packages
+export LD_LIBRARY_PATH="${SITE}/~-mpy.libs:${LD_LIBRARY_PATH}"
+
 
 echo "Starting saliency analysis (lgd_cm) on $(hostname)"
 echo "Job ID: $SLURM_JOB_ID"

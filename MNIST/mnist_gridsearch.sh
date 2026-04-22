1e98caa2c03dda4a6a781921613dfd6bdd3f4606
Ori Meidler <orineo1@gmail.com>
2026-04-22 23:20:14 +0300

    more var

--- MNIST/mnist_gridsearch.sh
+++ MNIST/mnist_gridsearch.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --partition=salmon
-#SBATCH --array=0-19
+#SBATCH --array=0-100
 
 REPO_ROOT="/sci/labs/orzuk/ori_m/conditional-matching-paper"
 ENV_PATH="/sci/labs/orzuk/ori_m/dps_env"

4bf682d1ea960f228a5a3065750d74bd8687e587
Ori Meidler <orineo1@gmail.com>
2026-04-22 22:06:51 +0300

    more advenced calssifer, cahnge the grid search

--- MNIST/train_classifiers.sh
+++ MNIST/train_classifiers.sh
@@ -10,10 +10,13 @@ REPO_ROOT="/sci/labs/orzuk/ori_m/conditional-matching-paper"
 ENV_PATH="/sci/labs/orzuk/ori_m/dps_env"
 
 source "$ENV_PATH/bin/activate" || { echo "ERROR activating env"; exit 1; }
+
 export REPO_ROOT="$REPO_ROOT"
+export HF_TOKEN="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"
 export WANDB_API_KEY="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"
+export CUBLAS_WORKSPACE_CONFIG=":4096:8"
 
 cd "$REPO_ROOT/MNIST"
-python mnist_gridsearch.py --config_id 0 --smoke_test
+python mnist_variance_sweep.py --train_classifier_only
 
-echo "Classifiers trained and saved."
+echo "Classifier trained and saved."

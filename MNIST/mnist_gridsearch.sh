2d7260e96871ab0c60d3f9e2804922ebcc7ce808
Ori Meidler <orineo1@gmail.com>
2026-04-22 12:38:57 +0300

    more grid for bimodal

--- MNIST/mnist_gridsearch.sh
+++ MNIST/mnist_gridsearch.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --partition=salmon
-#SBATCH --array=0-71
+#SBATCH --array=0-55
 
 REPO_ROOT="/sci/labs/orzuk/ori_m/conditional-matching-paper"
 ENV_PATH="/sci/labs/orzuk/ori_m/dps_env"

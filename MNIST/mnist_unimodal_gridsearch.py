1904c13854bd9e6f14b2959a1a4153215e76b3d2
Ori Meidler <orineo1@gmail.com>
2026-04-30 16:37:10 +0300

    more tests

--- MNIST/mnist_unimodal_gridsearch.py
+++ MNIST/mnist_unimodal_gridsearch.py
@@ -70,10 +70,10 @@ UNIMODAL_DIGITS_OF_INTEREST = [2, 3, 5, 7]
 # ─────────────────────────────────────────────────────────────────────────────
 # Grid
 # ─────────────────────────────────────────────────────────────────────────────
-STEP_SIZE_MODES          = [  'half', 'double', 'no_linear']
+STEP_SIZE_MODES          = [  'half', 'double', 'no_linear',"doubleLinear"]
 UNIMODAL_VAR_LIST        = [495,505,515,525]
-NUM_INFERENCE_STEPS_LIST = [100,200,300]
-NUM_X_T_LIST             = [3,10, 20]
+NUM_INFERENCE_STEPS_LIST = [110,120,130]
+NUM_X_T_LIST             = [3]
 
 CONFIGS = [
     {
@@ -317,6 +317,8 @@ def compute_step_size(r_t, t, mode):
         return 1.0 / (r_t + 1e-8) * 0.1
     elif mode == 'half':
         return 0.5 * (r_t / (1 + r_t**2) + 5 * t / 1000)
+    elif mode == 'doubleLinear':
+        return  (r_t / (1 + r_t**2) + 10 * t / 1000)
     elif mode == 'double':
         return 2.0 * (r_t / (1 + r_t**2) + 5 * t / 1000)
     elif mode == 'no_linear':
